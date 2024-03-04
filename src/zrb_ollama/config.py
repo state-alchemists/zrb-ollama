a11e484558a1d8b6ba293bd686f9b39195272c4a
gofrendi <gofrendiasgard@gmail.com>
2024-03-04 16:54:33 +0700

    update default model

--- src/zrb_ollama/config.py
+++ src/zrb_ollama/config.py
@@ -20,7 +20,7 @@ DEFAULT_OLLAMA_BASE_URL = os.getenv(
     "ZRB_OLLAMA_BASE_URL", "http://localhost:11434"
 )
 DEFAULT_OLLAMA_MODEL = os.getenv(
-    "ZRB_OLLAMA_DEFAULT_MODEL", "mistral:7b-instruct"
+    "ZRB_OLLAMA_DEFAULT_MODEL", "mistral:latest"
 )
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
