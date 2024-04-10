85b0deef89153379ccff2861a75e96f54b759dd3
gofrendi <gofrendiasgard@gmail.com>
2024-04-10 14:22:51 +0700

    Save changes on Wed Apr 10 14:22:51 WIB 2024

--- _cmd/prepare-venv.sh
+++
@@ -1,3 +0,0 @@
-echo "Install packages"
-poetry install --only main
-poetry install -E openai -E bedrock -E embedding-cpu
--- _cmd/test-playground.sh
+++
@@ -1,3 +0,0 @@
-source .venv/bin/activate
-export ZRB_DEFAULT_LLM_PROVIDER=openai
-zrb chat "In the state of the union, What did the president say about Ketanji Brown Jackson"
